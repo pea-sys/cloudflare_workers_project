d017d5603925e9bba5b409d1b5216dc84d1bc84e
pea-sys <pea98258@gmail.com>
2026-08-09 13:42:01 +0900

    routing

--- first-workers/src/index.ts
+++ first-workers/src/index.ts
@@ -13,22 +13,54 @@
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const method = request.method;
 		const url = new URL(request.url);
 		const path = url.pathname;
 
-		if (path === '/' || path === '/home') {
-			return new Response('ホームページ', {
-				headers: { 'Contet-Type': 'text/plain;charset=UTF-8' },
-			});
-		} else if (path === '/about') {
-			return new Response('お問い合わせページです', {
-				headers: { 'Contet-Type': 'text/plain;charset=UTF-8' },
-			});
-		} else {
-			return new Response('ページが見つかりません', {
-				status: 404,
-				headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
-			});
+		if (method === 'GET') {
+			if (path === '/' || path === '/home') {
+				return new Response('ホームページ', {
+					headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+				});
+			} else if (path === '/about') {
+				return new Response('お問い合わせページです', {
+					headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+				});
+			} else if (path.startsWith('/users/')) {
+				const userId = path.split('/')[2];
+				return new Response(`ユーザーID: ${userId}のプロフィールページです`, {
+					headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+				});
+			}
+		} else if (method === 'POST') {
+			if (path === '/users') {
+				return new Response('新規ユーザーを作成しました', {
+					status: 201,
+					headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+				});
+			} else if (path === '/login') {
+				return new Response('ログインしました', {
+					headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+				});
+			}
+		} else if (method === 'PUT') {
+			if (path.startsWith('/users/')) {
+				const userId = path.split('/')[2];
+				return new Response(`ユーザーID: ${userId}の情報を更新しました`, {
+					headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+				});
+			}
+		} else if (method === 'DELETE') {
+			if (path.startsWith('/users/')) {
+				const userId = path.split('/')[2];
+				return new Response(`ユーザーID: ${userId}を削除しました`, {
+					headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+				});
+			}
 		}
+		return new Response('ページが見つかりません', {
+			status: 404,
+			headers: { 'Content-Type': 'text/plain;charset=UTF-8' },
+		});
 	},
 } satisfies ExportedHandler<Env>;
